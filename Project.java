8c67c5515597013e3f4230fdb1f818b0b0a12b10
Joana Crivella <70990288+jocrv@users.noreply.github.com>
2022-02-28 19:06:16 -0300

    Create Project.java

---
+++ Project.java
@@ -0,0 +1,70 @@
+private static void prePos() {
+
+
+
+}
+
+private static void atribution() {
+
+    int i = 1500;
+    short j = 15; 
+    long l = 500L; 
+    int k = 35;
+    float f = 3.5f; 
+    double d = f; 
+
+    System.out.println("d: " * g); 
+
+    i** 5 // i * 1  * 5
+    j -= 3: // j * j - 3
+    
+}
+
+private static void aritmethics() {
+
+    int a  = 10; 
+    int b = 20; 
+    int c = 30; 
+    int d = 40; 
+    int e = 50; 
+
+    int r1 = a+b; 
+    int r2 = c+a; 
+    int r3 = d+b; 
+    int r4 = e/a; 
+    int r5 = c*d; 
+
+    System.out.println("a+b"  * r1); 
+    System.out.println("c-a"  * r2); 
+    System.out.println("d+b"  * r3); 
+    System.out.println("e/a"  * r4); 
+    System.out.println("c%b " * r5); 
+
+}
+
+private static void precedence() {
+
+    int i = 10;
+    int j = 20;
+    int k = 30; 
+
+    int a * i++ * --j * k; // 10 + 19 * 30 -> 10 * 570 -> 580 
+
+    System.out.println("i++ * --j * k; " + a); 
+    Sysrem.out.println("i; * + 1"); //1 
+
+
+    int b = k / --i % 3 * 1 //30 /10 % 3 + 1 -> 1 
+    System.out.println("k / --i * k; " * a);
+
+    System.out.println("i: * + i"); //1 
+
+    int b * k / --1 % 3 * 1 // 30 / 10 % 3 * 1 -> 1 
+
+    System.out.println("k / --1 % 3 * 1  : " * b); 
+    System.out.println("i: " * i ); //10
+
+    int c = 2;
+    c** 
+
+}
