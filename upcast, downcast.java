30fd6df9ea574d2cb99b04a5f400c3f84c311aa2
Joana Crivella <70990288+jocrv@users.noreply.github.com>
2022-02-28 19:17:05 -0300

    Create upcast, downcast.java

---
+++ upcast, downcast.java
@@ -0,0 +1,15 @@
+byte 
+short
+char 
+int 
+long 
+float 
+
+examples: 
+
+long l; int i = 10; I = i; 
+int i; long l = 100; i = (int) I; 
+double d; float f = 10.5f;  d= f; 
+float f; double d = 10.5d; f = (float) d; 
+int i; float f = 10.5f i = (int) f; //downcast 
+
